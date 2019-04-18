708ba90162f5a560340786e2883c239590521c4a
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-04-18 11:01:25 +0300

    wip: update enums

--- batchrun/enums.py
+++ batchrun/enums.py
@@ -2,10 +2,21 @@ from django.utils.translation import ugettext_lazy as _
 from enumfields import Enum
 
 
+class CommandType(Enum):
+    EXECUTABLE = 'executable'
+    DJANGO_MANAGE = 'django-manage'
+
+    class Labels:
+        EXECUTABLE = _('executable or script')
+        DJANGO_MANAGE = _('Django management command')
+
+
 class EventLogKind(Enum):
     STDOUT = 'stdout'
     STDERR = 'stderr'
 
     class Labels:
-        STDOUT = _('Standard output stream')
-        STDERR = _('Standard error stream')
+        # Labels of stdout and stderr are not translated, because it
+        # breaks the filtering in Django Admin
+        STDOUT = 'stdout'
+        STDERR = 'stderr'
