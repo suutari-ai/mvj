05db81cc2ebbbd79a1bd2b85e869074eaa9a8a7c
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-04-18 11:01:57 +0300

    wip: update enumfields stubs

--- batchrun/stubs/enumfields.pyi
+++ batchrun/stubs/enumfields.pyi
@@ -1,5 +1,7 @@
 import enum
-from typing import Sequence, Tuple
+from typing import Any, Sequence, Tuple, Type, Union
+
+from django.db import models
 
 
 class Enum(enum.Enum):
@@ -7,3 +9,14 @@ class Enum(enum.Enum):
     def choices(cls) -> Sequence[Tuple[str, str]]: ...
 
     def __str__(self) -> str: ...
+
+
+_StrOrEnum = Union[str, Type[Enum]]
+
+
+class EnumFieldMixin:
+    ...
+
+
+class EnumField(EnumFieldMixin, models.CharField[Enum, Any]):
+    def __init__(self, enum: _StrOrEnum, **kwargs: Any) -> None: ...
