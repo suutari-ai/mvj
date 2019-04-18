d5af450f7bc5c6204c9f38de02ee8a056015b19c
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-04-18 11:01:36 +0300

    wip: update model mixins

--- batchrun/model_mixins.py
+++ batchrun/model_mixins.py
@@ -1,42 +1,29 @@
+from typing import Any
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from safedelete.models import SafeDeleteModel
 
 
+class CleansOnSave(models.Model):
+    class Meta:
+        abstract = True
+
+    def save(self, *args: Any, **kwargs: Any) -> None:
+        self.clean()
+        super().save(*args, **kwargs)
+
+
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(
-        auto_now_add=True, verbose_name=_("Time created"))
+        auto_now_add=True, verbose_name=_('creation time'))
     modified_at = models.DateTimeField(
-        auto_now=True, verbose_name=_("Time modified"))
+        auto_now=True, verbose_name=_('modification time'))
 
     class Meta:
         abstract = True
 
 
 class TimeStampedSafeDeleteModel(TimeStampedModel, SafeDeleteModel):
-    class Meta:
-        abstract = True
-
-
-class NameModel(models.Model):
-    name = models.CharField(verbose_name=_("Name"), max_length=255)
-
-    class Meta:
-        abstract = True
-        ordering = ['name']
-
-    def __str__(self):
-        return self.name
-
-
-class ArchivableModel(models.Model):
-    # In Finnish: Arkistoitu
-    archived_at = models.DateTimeField(verbose_name=_("Time archived"), null=True, blank=True)
-    # In Finnish: Huomautus (arkistointi)
-    archived_note = models.TextField(verbose_name=_("Archived note"), null=True, blank=True)
-
-    def is_archived(self):
-        return bool(self.archived_at)
-
     class Meta:
         abstract = True
