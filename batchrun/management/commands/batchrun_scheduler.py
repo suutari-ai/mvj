7713443130dde469908dda4e1980edd74405fbba
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-04-30 09:43:10 +0300

    wip: types for mgmt cmd

--- batchrun/management/commands/batchrun_scheduler.py
+++ batchrun/management/commands/batchrun_scheduler.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from django.core.management.base import BaseCommand
 
 from ...scheduler import run_scheduler_loop
@@ -6,5 +8,5 @@ from ...scheduler import run_scheduler_loop
 class Command(BaseCommand):
     help = 'Batch Run Scheduler'
 
-    def handle(self, *args, **options):
+    def handle(self, *args: Any, **options: Any) -> None:
         run_scheduler_loop()
